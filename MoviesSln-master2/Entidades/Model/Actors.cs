0bd8b75e3c748506be75a20978fd50b21cec139e
VilaNova <Gustavo_Vila_nova@Dell.com>
2020-09-27 17:25:23 -0300

    version2

---
+++ MoviesSln-master2/Entidades/Model/Actors.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Entidades.Model
+{
+    public class Actors
+    {
+        [Key]
+        public int ActorId { get; set; }
+
+        [MaxLength(40)]
+        public string Name { get; set; }
+        public string DateOfBirth { get; set; }
+    }
+}
